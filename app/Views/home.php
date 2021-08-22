4f2da2d37db7228b0e6513f0474e1f3038bdd1eb
eyitayo <eyitayoogunbiyi@gmail.com>
2021-08-22 19:48:05 +0100

    fixed css problems

--- app/Views/home.php
+++ app/Views/home.php
@@ -5,6 +5,7 @@
 	<title>Welcome to Consultly</title>
 	<meta name="description" content="The small framework with powerful features">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href = "/styles.css"/>
 	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
 </head>
 <body>
