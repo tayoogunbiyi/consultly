29e8b6cc29e83168620e47c1dc9c618b94e41a7e
eyitayo <eyitayoogunbiyi@gmail.com>
2021-08-23 22:20:02 +0100

    add authGuard

---
+++ app/Filters/AuthGuard.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+
+class AuthGuard implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if (!session()->get('isLoggedIn')) {
+            session()->setFlashdata('msg', 'You need to be logged in to access that page');
+            return redirect()
+                ->to('/signin');
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+    }
+}
