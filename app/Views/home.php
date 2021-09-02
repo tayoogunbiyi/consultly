8e3d29dd0fe25dd1fb4073911cbd5cbf0a9c0fc8
eyitayo <eyitayoogunbiyi@gmail.com>
2021-09-02 23:07:13 +0100

    deploy homepage

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,23 +1,457 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-	<meta charset="UTF-8">
-	<title>Welcome to Consultly</title>
-	<meta name="description" content="The small framework with powerful features">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href = "/styles.css"/>
-	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Home</title>
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/styles.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 </head>
+
 <body>
+	<div class="row">
+		<div class="col-2" style="background-color: #C4C4C4;"></div>
+		<div class="col-8" style="background-color: #C4C4C4;">
+			<div class="row my-4">
+				<div class="col-lg-2"> <a class="mr-100" href="signin.html" class="font-weight-bolder custom-link-black">Home</a>
+				</div>
+				<div class="col-lg-2">
+					<a class="mr-100" href="signin.html" class="font-weight-bolder custom-link-black">Service</a>
+				</div>
+				<div class="col-lg-2">
+					<a class="mr-100" href="signin.html" class="font-weight-bolder custom-link-black">Booking</a>
+				</div>
+				<div class="col-lg-2">
+					<a class="mr-100" href="signin.html" class="font-weight-bolder custom-link-black">About Us</a>
+				</div>
+				<div class="col-lg-2">
+					<a class="mr-100" href="signin.html" class="font-weight-bolder custom-link-black">Contact</a>
+				</div>
 
-<h2>Home</h2>
+				<div class="col-12 mt-5"></div>
 
-<div>
-	<a href="/signin"> Sign In </a>
-<div>
-<div>
-	<a href="/register"> Sign Up </a>
-</div>
+				<div class="col-4 mt-4">
+					<h1>
+						Let’s start something big together.
+					</h1>
+				</div>
+				<div class="col-8"></div>
 
+				<div class="col-12 mt-2">
+					<span>The easier approach to reaching your business goals</span>
+				</div>
+
+				<div class="col-lg-4 my-4">
+					<input type="submit" value="Book a Consultation" class="custom-primary-btn btn">
+				</div>
+
+				<div class="col-8"></div>
+
+				<div class="col">
+					<h1>
+						<b>
+							5+
+						</b>
+					</h1>
+					<br>
+					<h5>
+						<b>
+							Years Of Experience
+						</b>
+					</h5>
+				</div>
+
+				<div class="col">
+					<h1>
+						<b>
+							100%
+						</b>
+					</h1>
+					<br>
+					<h5>
+						<b>
+							Satisfaction
+							Rate
+						</b>
+					</h5>
+				</div>
+
+				<div class="col">
+					<h1>
+						<b>
+							200+
+						</b>
+					</h1>
+					<br>
+					<h5>
+						<b>
+							Completed
+							Projects
+						</b>
+					</h5>
+				</div>
+			</div>
+		</div>
+		<div class="col-2" style="background-color: #C4C4C4;"></div>
+
+		<div class="col-12 my-4"></div>
+
+		<div class="container col-10">
+			<div class="row my-4">
+				<div class="col-12">
+					<div class="row">
+						<div class="col">
+							<h2><b>Who we are</b></h2>
+						</div>
+						<div class="col-4"></div>
+					</div>
+				</div>
+
+				<!-- If there is an image, we can indicate as a col instead of a col-4 so as to be responsive -->
+				<div class="col my-4 mx-1" style="background-color: #C4C4C4; border-radius: 10px; min-height: 200px; min-width: 200px;">
+				</div>
+
+				<div class="col my-4">
+					<span>
+						The Nigerian Academy of Engineering was inspired by the need for a small, focused
+						engineering
+						body
+						<br>
+						<br>
+						that would have the narrower objectives of the advancement and pursuit of excellence
+						in
+						Technology
+						and Engineering, and the provision
+						<br>
+						<br>
+						of a national platform for experts to pool their experience and insights and make
+						input into
+						public
+						and private technical policy.
+					</span>
+				</div>
+				<div class="col-2"></div>
+
+				<div class="col-12 my-4">
+					<h2><b>Our Services</b></h2>
+				</div>
+
+				<div class="col-sm-12 col-md-3 col-xl-3 mx-1 my-2" style="background-color: #EDEDED; border-radius: 10px;">
+					<div class="container row">
+						<div class="col-12 my-4">
+							<span style="height: 56px; width: 56px; background-color: #C4C4C4; border-radius: 50%; display: inline-block;"></span>
+						</div>
+						<div class="col-12">
+							<h6><b>Fellows Platform</b></h6>
+						</div>
+
+						<div class="col-12 my-4">
+							<span>
+								Log in as a member and have access to important material and information
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-12 col-md-3 col-xl-3 mx-1 my-2" style="background-color: #EDEDED; border-radius: 10px;">
+					<div class="container row">
+						<div class="col-12 my-4">
+							<span style="height: 56px; width: 56px; background-color: #C4C4C4; border-radius: 50%; display: inline-block;"></span>
+						</div>
+						<div class="col-12">
+							<h6><b>Fellows Platform</b></h6>
+						</div>
+
+						<div class="col-12 my-4">
+							<span>
+								Log in as a member and have access to important material and information
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-12 col-md-3 col-xl-3 mx-1 my-2" style="background-color: #EDEDED; border-radius: 10px;">
+					<div class="container row">
+						<div class="col-12 my-4">
+							<span style="height: 56px; width: 56px; background-color: #C4C4C4; border-radius: 50%; display: inline-block;"></span>
+						</div>
+						<div class="col-12">
+							<h6><b>Fellows Platform</b></h6>
+						</div>
+
+						<div class="col-12 my-4">
+							<span>
+								Log in as a member and have access to important material and information
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-12 col-md-3 col-xl-3 mx-1 my-2" style="background-color: #EDEDED; border-radius: 10px;">
+					<div class="container row">
+						<div class="col-12 my-4">
+							<span style="height: 56px; width: 56px; background-color: #C4C4C4; border-radius: 50%; display: inline-block;"></span>
+						</div>
+						<div class="col-12">
+							<h6><b>Fellows Platform</b></h6>
+						</div>
+
+						<div class="col-12 my-4">
+							<span>
+								Log in as a member and have access to important material and information
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-12 col-md-3 col-xl-3 mx-1 my-2" style="background-color: #EDEDED; border-radius: 10px;">
+					<div class="container row">
+						<div class="col-12 my-4">
+							<span style="height: 56px; width: 56px; background-color: #C4C4C4; border-radius: 50%; display: inline-block;"></span>
+						</div>
+						<div class="col-12">
+							<h6><b>Fellows Platform</b></h6>
+						</div>
+
+						<div class="col-12 my-4">
+							<span>
+								Log in as a member and have access to important material and information
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-12 col-md-3 col-xl-3 mx-1 my-2" style="background-color: #EDEDED; border-radius: 10px; display: inline-block;">
+					<div class="container row">
+						<div class="col-12 my-4">
+							<span style="height: 56px; width: 56px; background-color: #C4C4C4; border-radius: 50%; display: inline-block;"></span>
+						</div>
+						<div class="col-12">
+							<h6><b>Fellows Platform</b></h6>
+						</div>
+
+						<div class="col-12 my-4">
+							<span>
+								Log in as a member and have access to important material and information
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-12 mt-4"></div>
+				<div class="col-12 mt-4 mb-2">
+					<h2>
+						<b>Reviews</b>
+					</h2>
+				</div>
+
+				<div class="col-12">
+					<div class="row">
+						<div class="col-12">
+							<div class="col" style="background-color: #C4C4C4; border-radius: 10px; height: 500px; width: 400px;">
+							</div>
+							<div class="col">
+								<h4 class="my-4"><b>“</b></h4>
+								<br>
+								<span class="my-2">
+									<b> The Nigerian Academy of Engineering was inspired by the need for a small,
+										focused
+										engineering
+										body that would have the narrower objectives of the advancement and pursuit of
+										excellence in
+										Technology and Engineering, and the provision
+									</b>
+								</span>
+								<br>
+								<p class="my-1">OSHIN Oluwatomisin</p>
+								<br>
+								<span class="my-1">CEO Nowhere.org</span>
+								<br>
+								<button type="button" class="btn btn-sm my-1"><i class="fa fa-arrow-left" style="background-color: #C4C4C4; width: 30px;
+                                        height: 30px;
+                                        padding: 6px 0px;
+                                        border-radius: 50%;
+                                        text-align: center;
+                                        font-size: 12px;
+                                        line-height: 1.42857;"></i>
+								</button>
+
+								<button type="button" class="btn btn-sm my-1"><i class="fa fa-arrow-right" style="background-color: #C4C4C4; width: 30px;
+                                    height: 30px;
+                                    padding: 6px 0px;
+                                    border-radius: 50%;
+                                    text-align: center;
+                                    font-size: 12px;
+                                    line-height: 1.42857;"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 my-4"></div>
+
+				<div class="col-12">
+					<h1> <b> FAQ's </b></h1>
+				</div>
+				<div class="col-lg-8 my-4" id="accordion">
+
+					<div class="card">
+						<div class="card-header">
+							<a class="card-link" data-toggle="collapse" href="#collapseOne">
+								<b> How do I book my consultation</b>
+							</a>
+						</div>
+						<div id="collapseOne" class="collapse" data-parent="#accordion">
+							<div class="card-body">
+								Lorem ipsum..
+							</div>
+						</div>
+					</div>
+
+					<div class="card">
+						<div class="card-header">
+							<a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
+								<b> How do I book my consultation</b>
+							</a>
+						</div>
+						<div id="collapseTwo" class="collapse" data-parent="#accordion">
+							<div class="card-body">
+								Lorem ipsum..
+							</div>
+						</div>
+					</div>
+
+					<div class="card">
+						<div class="card-header">
+							<a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
+								<b> How do I book my consultation</b>
+							</a>
+						</div>
+						<div id="collapseThree" class="collapse" data-parent="#accordion">
+							<div class="card-body">
+								Lorem ipsum..
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-lg-4"></div>
+			</div>
+
+			<div class="col-10">
+				<div class="row" style="background-color: #C4C4C4; border-radius: 10px;">
+					<div class="col-12 my-4"></div>
+
+					<div class="col-12">
+						<div class="row">
+							<div class="col-2"></div>
+							<div class="col ml-auto">
+								<h5>
+									<b>
+										Have any questions
+										<br> <br> Feel free to Contact Us
+									</b>
+								</h5>
+							</div>
+
+							<div class="col-lg-2 my-auto">
+								<input type="submit" value="Contact Us" class="custom-primary-btn btn">
+							</div>
+							<div class="col-2"></div>
+						</div>
+					</div>
+
+					<div class="col-12 my-4"></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-12 mt-4" style="background-color: #C4C4C4;">
+			<div class="row ">
+				<div class="col mt-4 ml-4 mr-auto">
+					<h3>
+						<b>
+							Company Name
+						</b>
+					</h3>
+				</div>
+
+				<div class="col mt-4">
+					<div class="row">
+						<div class="col mb-4">
+							<div class="row">
+								<div class="col-12">
+									<span><b>Contact Us</b></span>
+								</div>
+
+								<div class="col-12">
+									<span>+234 810 999 2709</span>
+								</div>
+
+								<div class="col-12 mt-4">
+									<span><b>Office</b></span>
+								</div>
+
+								<div class="col-12 mb-4">
+									<span>CITS, Unilag road, akoka, yaba, Lagos</span>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="col mb-4">
+							<div class="row">
+								<div class="col-12">
+									<span><b>Services</b></span>
+								</div>
+
+								<div class="col-12">
+									<span>Services</span>
+								</div>
+
+								<div class="col-12">
+									<span>Why Us</span>
+								</div>
+
+								<div class="col-12">
+									<span>Pricing</span>
+								</div>
+
+								<div class="col-12">
+									<span>Testimonials</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="col mb-4">
+							<div class="row">
+								<div class="col-12">
+									<span><b>Resources</b></span>
+								</div>
+
+								<div class="col-12">
+									<span>Terms & Conditions</span>
+								</div>
+
+								<div class="col-12">
+									<span>Help</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer class="page-footer font-small" style="background-color: #000000;">
+		<div class="footer-copyright text-center py-3" style="color: white;">
+			Copywright © 2021. All rights reserved
+		</div>
+	</footer>
 </body>
+
 </html>
